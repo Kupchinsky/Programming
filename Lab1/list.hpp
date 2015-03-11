eb68c7b4f689c120470bae6e41c054e349de3037
Kupchinsky <user@killer666.ru>
2015-03-11 22:04:30 +0700

    Added comments

--- Lab1/list.hpp
+++ Lab1/list.hpp
@@ -60,21 +60,41 @@ public:
     ~List();
 
     void swapNodes(unsigned int index1, unsigned int index2);
+
+	// 2.1. Напишите функцию, вставляющую узел в начало связанного списка и функцию, удаляющую этот узел.
     void pushFirst(_LIST_TYPE);
     void deleteFirst();
+	//
+
     void deleteNode(ListNode*);
+
+	// 2.2. Повторите предыдущее задание, на этот раз вставляя и удаляя не первый, а последний элемент списка.
+	// 2.3. Повторите предыдущее задание, считая что существуют указатели на голову и хвост списка.
     void pushLast(_LIST_TYPE);
     void deleteLast();
+	//
 
     _LIST_TYPE get(unsigned int index);
     _LIST_TYPE first();
     _LIST_TYPE last();
 
+	// 3. Напишите функцию для подсчета количества узлов в связанном списке.
+	// 3.1. Итеративно.
     unsigned int calculateSize();
-    unsigned int calculateSizeRecursive();
+	//
 
+	// 3.2. Рекурсивно.
+    unsigned int calculateSizeRecursive();
+	//
+
+	// 4. Напишите функцию для удаления узла, содержащего наибольшее целое число, из связанного списка целых чисел.
     void deleteMostGreaterNode();
+	//
+
+	// Слияние упорядоченных списков
     List* mergeLists(List*);
+	//
+
     void print(std::ostream*);
 };
 
