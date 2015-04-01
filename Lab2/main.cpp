c3ae7f7ff52414dfd159e77d60a739e78055d4b1
Kupchinsky <user@killer666.ru>
2015-04-01 17:41:32 +0700

    First release (task 6)

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -4,40 +4,98 @@
 
 using namespace std;
 
+#define _DEBUG 0
 #define out(x) wcout << x << endl
 
-struct User
-{
-    int timeArrive, timeServed;
-};
+#if _DEBUG > 0
+#define trace(x) wcout << "     " << x << endl
+#else
+#define trace(x)
+#endif
 
-#include "..\Lab1\list.hpp"
+struct Visitor
+{
+    int timeArrive, duration;
+};
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    // Testing queues
-    Queue<int> *q1 = new Queue<int>();
-
-    for (int i = 0; i < 5; i++)
-        q1->add(i);
-
-    for (int i = 0; i < 5; i++)
-        out(i << ": " << q1->poll());
-    //
-
-    List<User> *users = new List<User>();
+    Queue<Visitor> *visitors = new Queue<Visitor>(), *arrivedVisitors = new Queue<Visitor>();
 
     ifstream ifs("..\\input.txt", ios::in);
 
     while (!ifs.eof())
     {
-        User newUser;
+        Visitor newVisitor;
 
-        ifs >> newUser.timeArrive >> newUser.timeServed;
-        users->pushLast(newUser);
+        ifs >> newVisitor.timeArrive >> newVisitor.duration;
+        visitors->add(newVisitor);
     }
+
+    // Задание
+    out(L"Начало моделирования...");
+    int
+            currentTime = 0,   // Текущее время
+            nextTime = 0,      // Следующее время отбытия посетителя
+            waitTime = 0,      // Общее время ожидания
+            visitorsCount = 0; // Количество посетителей
+
+    while (visitors->size() > 0 || arrivedVisitors->size() > 0)
+    {
+        currentTime++;
+
+        trace(L"Текущее время: " << currentTime);
+
+        // Прибытие
+        if (visitors->size() > 0 && visitors->peek().timeArrive == currentTime) // Время прибытия равно текущему времени
+        {
+            Visitor current = visitors->poll();
+
+            arrivedVisitors->add(current); // Добавляем в очередь прибывших
+            visitorsCount++; // Количество посетителей стало больше
+
+            if (nextTime == 0)
+                nextTime = current.timeArrive;
+
+            out(L"Обработка прибытия в момент: " << currentTime);
+        }
+
+        // Отбытие
+        if (arrivedVisitors->size() > 0)
+        {
+            trace(L"Очередь не пустая, проверяем");
+            Visitor current = arrivedVisitors->peek();
+
+            if (nextTime + current.duration == currentTime)
+            {
+                nextTime += current.duration;
+                trace("nextTime = " << nextTime);
+
+                arrivedVisitors->poll(); // Удаляем посетителя
+
+                out(L"Обработка отбытия в момент: " << currentTime);
+            }
+            else
+                trace(L"Посетитель отбудет в момент времени " << nextTime);
+
+            //if (arrivedVisitors->size() > 1)
+            //    waitTime++;
+        }
+        else
+        {
+            if (nextTime > 0) // Посетители отсутствуют, но время следующего не сброшено
+            {
+                nextTime = 0; // Время отбытия обнуляем
+                trace("nextTime = 0");
+            }
+        }
+    }
+
+    out(L"Конец моделирования" << endl
+        << L"   Общее количество посетителей: " << visitorsCount << endl
+        << L"   Среднее время ожидания в очереди: " << waitTime);
 
     return 0;
 }
