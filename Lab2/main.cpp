a44092638b446553a3ddede0436fb76c18f2fe27
Kupchinsky <user@killer666.ru>
2015-04-20 09:42:44 +0700

    updated

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -31,8 +31,10 @@ int main()
     out(L"Начало моделирования...");
     int
             currentTime = 0,   // Текущее время
-            nextTime = 0,      // Время прибытия последнего посетителя
+            nextTime = 0,      // Следующее время отбытия посетителя
+            waitTime = 0,      // Время ожидания конкретного посетителя
             allWaitTime = 0,   // Общее время ожидания
+            waitVisitors = 0, // Количество посетителей, которые ожидали очередь
             visitorsCount = 0; // Количество посетителей
 
     while (visitors->size() > 0 || arrivedVisitors->size() > 0)
@@ -57,6 +59,7 @@ int main()
         if (arrivedVisitors->size() > 0)
         {
             Visitor current = arrivedVisitors->peek();
+            bool minusOne = true;
 
             if (nextTime + current.duration == currentTime)
             {
@@ -64,21 +67,31 @@ int main()
                 arrivedVisitors->poll(); // Удаляем посетителя
 
                 out(L"Обработка отбытия в момент: " << currentTime);
+                out(L"Кто-то ещё ждал: " << waitTime);
+
+                if (waitTime > 0)
+                {
+                    out(L"Ждало: " << arrivedVisitors->size());
+                    waitVisitors += arrivedVisitors->size();
+                    allWaitTime += waitTime;
+                }
+
+                waitTime = 0;
+                minusOne = false;
             }
 
-            if (arrivedVisitors->size() > 1) // Есть ожидающие посетители
-                allWaitTime += arrivedVisitors->size() - 1; // Прибавляем общее время ожидания, не считая того, кто обслуживается
+            waitTime += arrivedVisitors->size() - (minusOne ? 1 : 0);
         }
         else
         {
-            if (nextTime > 0) // Посетители отсутствуют, но время не сброшено
-                nextTime = 0;
+            if (nextTime > 0) // Посетители отсутствуют, но время следующего не сброшено
+                nextTime = 0; // Время отбытия обнуляем
         }
     }
 
     out(L"Конец моделирования" << endl
         << L"   Общее количество посетителей: " << visitorsCount << endl
-        << L"   Среднее время ожидания в очереди: " << allWaitTime / (float) visitorsCount);
+        << L"   Среднее время ожидания в очереди: " << allWaitTime - 1 << " " << waitVisitors);
 
     return 0;
 }
